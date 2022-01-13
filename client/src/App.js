64bd88a0822d3b1f1724a2ef9986653c3fe88c8c
Diane Galiano <dianegaliano09@gmail.com>
2022-01-12 19:52:56 -0600

    added code to the index.js file in the PartsItem folder

--- client/src/App.js
+++ client/src/App.js
@@ -26,22 +26,22 @@ uri: '/graphql',
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Provider store={store}>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/products/:id" component={Detail} />
+          </Switch>
+        </Provider>
+      </div>
+    </Router>
+  </ApolloProvider>
   );
 }
 
