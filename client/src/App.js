e6a5ab5108cb20c04d9a0c0f5822a79ccdd88457
Diane Galiano <dianegaliano09@gmail.com>
2022-01-12 19:40:25 -0600

    working on components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+//Import components
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+import OrderHistory from './pages/OrderHistory';
+
+import store from './utils/store';
+import { Provider } from 'react-redux';
+
+const client = new ApolloClient({ 
+  request: (operation) => { 
+  const token = localStorage.getItem('id_token');
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  })
+},
+uri: '/graphql',
+});
 
 function App() {
   return (
