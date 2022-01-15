30d4687f814a4bba288153f120766119651c7b58
Diane Galiano <dianegaliano09@gmail.com>
2022-01-15 13:40:50 -0600

    fixing cart style  errors

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -7,6 +7,7 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
