52dd9b5a935bb716612ce465a9b6e45793cdf48f
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2022-01-15 11:01:03 -0600

    pushing code to main

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-//Import components
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import OrderHistory from './pages/OrderHistory';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
-import store from './utils/store';
-import { Provider } from 'react-redux';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+import OrderHistory from "./pages/OrderHistory";
 
-const client = new ApolloClient({ 
-  request: (operation) => { 
-  const token = localStorage.getItem('id_token');
-  operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : '',
-    }
-  })
-},
-uri: '/graphql',
+import store from "./utils/store";
+import { Provider } from "react-redux";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <div>
-        <Provider store={store}>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-          </Switch>
-        </Provider>
-      </div>
-    </Router>
-  </ApolloProvider>
+      <Router>
+        <div>
+          <Provider store={store}>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+            </Switch>
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
