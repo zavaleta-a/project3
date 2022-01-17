6e40498883a4856741b5cbf2c763a3823f6163ae
Diane Galiano <dianegaliano09@gmail.com>
2022-01-17 17:11:46 -0600

    deploy

--- client/src/components/PartsList/index.js
+++ client/src/components/PartsList/index.js
@@ -1,72 +1,71 @@
 import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-// import { useStoreContext } from '../../utils/GlobalState';
+import PartsItem from "../PartsItem";
 import { useDispatch, useSelector } from 'react-redux';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { UPDATE_PARTS} from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_PRODUCTS } from '../../utils/queries';
+import { QUERY_PARTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
-function ProductList() {
+function PartsList() {
   const dispatch = useDispatch();
   const state = useSelector(state => state);
 
   const { currentCategory } = state;
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_PARTS);
 
   useEffect(() => {
     if (data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
+        type: UPDATE_PARTS,
+        parts: data.parts,
       });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+      data.parts.forEach((part) => {
+        idbPromise('parts', 'put', part);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise('parts', 'get').then((parts) => {
         dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
+          type: UPDATE_PARTS,
+          parts: parts,
         });
       });
     }
   }, [data, loading, dispatch]);
 
-  function filterProducts() {
+  function filterParts() {
     if (!currentCategory) {
-      return state.products;
+      return state.parts;
     }
 
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
+    return state.parts.filter(
+      (part) => part.category._id === currentCategory
     );
   }
 
   return (
     <div className="my-2">
-      <h2>Our Products:</h2>
-      {state.products.length ? (
+      <h2>Our Parts:</h2>
+      {state.parts.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
+          {filterParts().map((parts) => (
+            <PartsItem
+              key={parts._id}
+              _id={parts._id}
+              image={parts.image}
+              name={parts.name}
+              price={parts.price}
+              quantity={parts.quantity}
             />
           ))}
         </div>
       ) : (
-        <h3>You haven't added any products yet!</h3>
+        <h3>You haven't added any parts yet!</h3>
       )}
       {loading ? <img src={spinner} alt="loading" /> : null}
     </div>
   );
 }
 
-export default ProductList;
+export default PartsList;
